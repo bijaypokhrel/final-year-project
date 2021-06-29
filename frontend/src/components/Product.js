475846e37685b2c9eb38b72ab8ac4d3b881791f9
Bijay Pokhrel <pokhrelbijay44@gmail.com>
2021-06-29 18:10:11 +0545

    Rating component update

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,20 +1,20 @@
-import React from 'react'
+import React from 'react';
+import Rating from './Rating';
 
 export default function Product(props) {
-    const {product} = props;
-    return (
-        <div key={product._id} className="card">
-                    <a className="medium" href={`/product/${product._id}`}><img src={product.image} alt={product.name} width="300" height="450"/>
-                    </a>
-                    <div className="card-body">
-                      <a className="medium" href={`/product/${product._id}`}>
-                        <h2>{product.name}</h2>
-                      </a>
-
-                        <div className="price">
-                            Rs{product.price}
-                        </div>
-                    </div>
-                </div>
-    )
+	const { product } = props;
+	return (
+		<div key={product._id} className='card'>
+			<a className='medium' href={`/product/${product._id}`}>
+				<img src={product.image} alt={product.name} width='300' height='450' />
+			</a>
+			<div className='card-body'>
+				<a className='medium' href={`/product/${product._id}`}>
+					<h2>{product.name}</h2>
+				</a>
+				<Rating rating={product.rating} numReviews={product.numReviews} />
+				<div className='price'>Rs{product.price}</div>
+			</div>
+		</div>
+	);
 }
